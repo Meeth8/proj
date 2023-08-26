51f97d73f2a54e56a791cc0bace20173c56586bc
Meeth <luniyameeth9@gmail.com>
2023-08-26 16:43:01 +0530

    sll

---
+++ sll.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+class SinglyLinkedList {
+private:
+    Node* head;
+
+public:
+    SinglyLinkedList() {
+        head = nullptr;
+    }
+
+    void append(int val) {
+        Node* newNode = new Node(val);
+
+        if (head == nullptr) {
+            head = newNode;
+        } else {
+            Node* current = head;
+            while (current->next != nullptr) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+    }
+
+    void display() {
+        Node* current = head;
+        while (current != nullptr) {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    SinglyLinkedList list;
+    list.append(1);
+    list.append(2);
+    list.append(3);
+
+    cout << "Singly Linked List: ";
+    list.display();
+
+    return 0;
+}
